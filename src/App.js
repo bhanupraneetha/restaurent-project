b7ed691f3425f94a47d1eb4b994cfdff7caf7096
bhanupraneetha1234 <134360530+bhanupraneetha@users.noreply.github.com>
2024-02-05 13:30:27 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-// import React,{ useState }  from 'react'
+import React from 'react'
 import FirstPage from './components/FirstPage/FirstPage';
 import SecondPage from './components/SecondPage/SecondPage';
-// import ThirdPage from './components/BigCard/ThirdPage/ThirdPage';
-// // import FourthPage from './components/FourthPage/FourthPage'
+import ThirdPage from './components/ThirdPage/ThirdPage';
  import FourthPage from './components/FourthPage/FourthPage';
 import FifthPage from './components/FifthPage/FifthPage';
 import SixthPage from './components/SixthPage/SixthPage';
 import SeventhPage from './components/SeventhPage/SeventhPage'
-import React from 'react'
-import ThirdPage from './components/ThirdPage/ThirdPage'
+
+
 function App() {
   return (
     <div>
